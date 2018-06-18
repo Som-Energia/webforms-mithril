72528e240ec6836a40444c355d9e55d6e678105e
David García Garzón <voki@canvoki.net>
2018-06-18 18:41:04 +0200

    extracted statecity from newmember

--- app/newmember.js
+++ app/newmember.js
@@ -5,8 +5,9 @@ var css = require('./style.styl');
 var Layout = require('./layout');
 var Row = Layout.Row;
 var Cell = Layout.Cell;
-var ValidatedInput = require('./validatedinput');
 var Select = require('./select');
+var ValidatedInput = require('./validatedinput');
+var StateCityChooser = require('./statecity');
 require('@material/button/dist/mdc.button.css');
 require('font-awesome/css/font-awesome.css');
 
@@ -19,126 +20,11 @@ var Persona = {
 	nifValidation: {},
 };
 
-
-var requestSom = require('./somapi').requestSom;
-
-var StateCityChooser = {
-	oninit: function(vn) {
-		var self = this;
-		self.states = []; // TODO: Shared by instances
-		self.stateIndex = 0;
-		self.state = undefined;
-		self.stateError = undefined;
-		self.city = undefined;
-		self.cityError = undefined;
-		self.cities = [];
-
-		self.updateStates();
-	},
-
-	updateStates: function(countryid) {
-		var self = this;
-		requestSom('/data/provincies').then(function(data) {
-			self.states = data.data.provincies;
-		}).catch(function(reason) {
-			console.log("TODO: Failed", reason);
-			self.stateError = _('Error loading states');
-		});
-	},
-
-	updateCities: function(stateid) {
-		var self = this;
-		self.cities = [];
-		self.city = undefined;
-		var statename = self.states.find(function(v) {return v.id==stateid;}).name;
-		self.cityError = _('Loading municipalities in %{statename}',{statename:statename});
-		requestSom('/data/municipis/'+stateid).then(function(data) {
-			self.city = undefined;
-			self.cityError = undefined;
-			self.cities = data.data.municipis;
-		}).catch(function(reason) {
-			console.log("TODO: Failed", reason);
-			self.cityError = _('Error loading cities');
-		});
-	},
-
-	view: function(vn) {
-		var self=this;
-		return m(Row, [
-			m(Cell, {span:6},
-				m(Select, {
-					options: this.states.map(function(v) {
-						return {
-							value: v.id,
-							text: v.name,
-						};
-					}),
-					label: _('State'),
-					help: this.stateError?this.stateError:(this.states?_('Choose a state'):_('Loading...')),
-					required: true,
-					value: self.state,
-					onchange: function (ev) {
-						self.state = ev.target.value;
-						self.updateCities(self.state);
-						vn.attrs.onchange && vn.attrs.onchange(self)
-					},
-				})
-			),
-			m(Cell, {span:6},
-				m(Select, {
-					options: this.cities.map(function(v) {
-						return {
-							value: v.id,
-							text: v.name,
-						};
-					}),
-					label: _('City'),
-					help: this.cityError?this.cityError:(this.cities?_('Choose a city'):_('Loading...')),
-					required: true,
-					value: self.city || '',
-					onchange: function (ev) {
-						self.city = ev.target.value;
-						vn.attrs.onchange && vn.attrs.onchange(self)
-					},
-				})
-			),
-		]);
-	},
-};
-
-
 var PersonalDataEditor = {
 	view: function(vn) {
 		return m(Layout, [
 			m(StateCityChooser),
 			m(Row, [
-				m(Cell,
-					m(Select, {
-						id: 'state',
-						label: _('State'),
-						help: _('Select the state'),
-						required: true,
-						//disabled: true,
-						value: Persona.gustos,
-						onchange: function(ev) {
-							Persona.gustos = ev.target.value;
-						},
-						options: [
-							{
-								value: 'grains',
-								text: _('Bread, Cereal, Rice, and Pasta'),
-							},
-							{
-								value: 'vegetables',
-								text: _('Vegetables'),
-							},
-							{
-								value: 'fruits',
-								text: _('Apple'),
-							},
-						],
-					})
-				),
 				m(Cell,
 					m(ValidatedInput, {
 						id: 'caixa1',
---
+++ app/statecity.js
@@ -0,0 +1,106 @@
+'use strict';
+var m = require('mithril');
+var _ = require('./translate');
+var Layout = require('./layout');
+var Row = Layout.Row;
+var Cell = Layout.Cell;
+var Select = require('./select');
+var requestSom = require('./somapi').requestSom;
+
+
+var Persona = {
+	field: undefined,
+	name: undefined,
+	nif: undefined,
+	nifValidation: {},
+};
+
+
+var StateCityChooser = {
+	oninit: function(vn) {
+		var self = this;
+		self.states = []; // TODO: Shared by instances
+		self.stateIndex = 0;
+		self.state = undefined;
+		self.stateError = undefined;
+		self.city = undefined;
+		self.cityError = undefined;
+		self.cities = [];
+
+		self.updateStates();
+	},
+
+	updateStates: function(countryid) {
+		var self = this;
+		requestSom('/data/provincies').then(function(data) {
+			self.states = data.data.provincies;
+		}).catch(function(reason) {
+			console.log("TODO: Failed", reason);
+			self.stateError = _('Error loading states');
+		});
+	},
+
+	updateCities: function(stateid) {
+		var self = this;
+		self.cities = [];
+		self.city = undefined;
+		var statename = self.states.find(function(v) {return v.id==stateid;}).name;
+		self.cityError = _('Loading municipalities in %{statename}',{statename:statename});
+		requestSom('/data/municipis/'+stateid).then(function(data) {
+			self.city = undefined;
+			self.cityError = undefined;
+			self.cities = data.data.municipis;
+		}).catch(function(reason) {
+			console.log("TODO: Failed", reason);
+			self.cityError = _('Error loading cities');
+		});
+	},
+
+	view: function(vn) {
+		var self=this;
+		return m(Row, [
+			m(Cell, {span:6},
+				m(Select, {
+					options: this.states.map(function(v) {
+						return {
+							value: v.id,
+							text: v.name,
+						};
+					}),
+					label: _('State'),
+					help: this.stateError?this.stateError:(this.states?_('Choose a state'):_('Loading...')),
+					required: true,
+					value: self.state,
+					onchange: function (ev) {
+						self.state = ev.target.value;
+						self.updateCities(self.state);
+						vn.attrs.onchange && vn.attrs.onchange(self)
+					},
+				})
+			),
+			m(Cell, {span:6},
+				m(Select, {
+					options: this.cities.map(function(v) {
+						return {
+							value: v.id,
+							text: v.name,
+						};
+					}),
+					label: _('City'),
+					help: this.cityError?this.cityError:(this.cities?_('Choose a city'):_('Loading...')),
+					required: true,
+					value: self.city || '',
+					onchange: function (ev) {
+						self.city = ev.target.value;
+						vn.attrs.onchange && vn.attrs.onchange(self)
+					},
+				})
+			),
+		]);
+	},
+};
+
+
+module.exports = StateCityChooser;
+
+// vim: noet ts=4 sw=4
