378f0cb9839013baecac9b4b3f4f7f8e456e47f2
David García Garzón <voki@canvoki.net>
2018-07-30 02:37:13 +0200

    linearprogress enhanced examples

--- src/mdc/linearprogress.js
+++ src/mdc/linearprogress.js
@@ -30,11 +30,11 @@ LinearProgress.view = function(vn) {
 		, vn.attrs, [
 		m('.mdc-linear-progress__buffering-dots'),
         m('.mdc-linear-progress__buffer',
-			vn.attrs.color2?{style: {'background-color': vn.attrs.color2}}:{}
+			vn.attrs.color2 && {style: {'background-color': vn.attrs.color2}}
 		),
 		m('.mdc-linear-progress__bar.mdc-linear-progress__primary-bar',
 			m('span.mdc-linear-progress__bar-inner',
-				vn.attrs.color?{style: {'background-color': vn.attrs.color}}:{}
+				vn.attrs.color && {style: {'background-color': vn.attrs.color}}
 			)
 		),
 		m('.mdc-linear-progress__bar mdc-linear-progress__secondary-bar',
@@ -46,23 +46,25 @@ LinearProgress.view = function(vn) {
 LinearProgress.Example = {};
 LinearProgress.Example.oninit = function(vn) {
 	this.value1 = 0;
+	function tick() {
+		vn.state.value1++;
+		vn.state.value1%=51;
+		setTimeout(tick, 2000);
+		m.redraw();
+	}
+	tick();
 };
 LinearProgress.Example.view = function(vn) {
-	setTimeout(function() {
-		vn.state.value1%=50;
-		vn.state.value1++;
-		m.redraw();
-	}, 200);
 	var Layout = require('./layout');
 	return m(Layout,
-		m(Layout.Row, m(Layout.Cell, m('h2', 'Linear Progress'))),
+		m(Layout.Row, m(Layout.Cell, m('h2', 'Linear Progress '))),
 		m(Layout.Row, [
-			m(Layout.Cell, {span: 12}, 'Indeterminate', m(LinearProgress)),
+			false && m(Layout.Cell, {span: 12}, 'Indeterminate', m(LinearProgress)),
 			m(Layout.Cell, {span: 12}, 'Determinate', m(LinearProgress, {max:50, value: vn.state.value1})),
 			m(Layout.Cell, {span: 12}, 'Buffer', m(LinearProgress, {max:50, buffer: vn.state.value1-vn.state.value1%3, value: vn.state.value1/4*3}), 'Undertext'),
 			m(Layout.Cell, {span: 12}, 'Reversed', m(LinearProgress, {reversed: true, max:50, value: vn.state.value1})),
 			m(Layout.Cell, {span: 12}, 'Colored', m(LinearProgress, {max:50, value: vn.state.value1, color: 'red', color2: 'orange'})),
-			m(Layout.Cell, {span: 12}, 'Colored Buffer', m(LinearProgress, {color: 'red', color2: 'orange', color3: 'blue', max:50, buffer: vn.state.value1-vn.state.value1%3, value: vn.state.value1/4*3})),
+			m(Layout.Cell, {span: 12}, 'Colored Buffer', m(LinearProgress, {color: 'red', color2: 'orange', max:50, buffer: vn.state.value1-vn.state.value1%3, value: vn.state.value1/4*3})),
 			m(Layout.Cell, {span: 12}, 'Wide', m(LinearProgress, {style: 'height: 8pt;', max:50, value: vn.state.value1})),
 		]),
 	);
