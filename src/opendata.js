e2990b9fe597a23dbd3e6071e0097b39851538de
David García Garzón <voki@canvoki.net>
2018-10-04 00:40:33 +0200

    opendata: table view

--- src/opendata.js
+++ src/opendata.js
@@ -24,6 +24,7 @@ var geolevel = '';
 var fromdate = undefined;
 var todate = undefined;
 var ondate = undefined;
+var viewmode= 'table';
 
 function uri() {
     var result = uribase+'/'+metric;
@@ -133,49 +134,53 @@ var OpenData = {
                     value: 'weekly',
                 }],
             }),
-			time==='on' && m(DatePicker, {
-				id: 'ondate',
-				label: _('On'),
-				help: _(
-					'Date at which look for. '+
-					'Default: last available.'),
-				value: ondate,
-				onchange: function(newvalue) {
-					ondate=newvalue;
-				},
-				boxed: true,
-				autoclose: true,
-			}),
+			m(Layout.Row, [
+				time==='on' && m(Layout.Cell, {span:6, spantablet:8}, m(DatePicker, {
+					id: 'ondate',
+					label: _('On'),
+					required: true,
+					help: _(
+						'Date at which look for. '+
+						'Default: last available.'),
+					value: ondate,
+					onchange: function(newvalue) {
+						ondate=newvalue;
+					},
+					boxed: true,
+					autoclose: true,
+				})),
 
-			time !== 'on' && m(DatePicker, {
-				id: 'fromdate',
-				label: _('From'),
-				help: _(
-					'First day that will be included. '+
-					'Default: oldest available.'),
-				value: fromdate,
-				onchange: function(newvalue) {
-					fromdate=newvalue;
-				},
-				boxed: true,
-				autoclose: true,
-			}),
+				time !== 'on' && m(Layout.Cell, {span:6, spantablet:8}, m(DatePicker, {
+						id: 'fromdate',
+						label: _('From'),
+						help: _(
+							'First day that will be included. '+
+							'Default: oldest available.'),
+						value: fromdate,
+						onchange: function(newvalue) {
+							fromdate=newvalue;
+						},
+						boxed: true,
+						autoclose: true,
+					})),
 
-			time !== 'on' && m(DatePicker, {
-				id: 'todate',
-				label: _('To'),
-				help: _(
-					'Last day that will be included. '+
-					'Default: last available'),
-				value: todate,
-				onchange: function(newvalue) {
-					todate=newvalue;
-				},
-				boxed: true,
-				autoclose: true,
-			}),
+				time !== 'on' && m(Layout.Cell, {span:6, spantablet:8}, m(DatePicker, {
+					id: 'todate',
+					label: _('To'),
+					help: _(
+						'Last day that will be included. '+
+						'Default: last available'),
+					value: todate,
+					onchange: function(newvalue) {
+						todate=newvalue;
+					},
+					boxed: true,
+					autoclose: true,
+				})),
+			]),
 
-            m('',
+			m('', {style: 'text-align: center'},
+				m('', m('tt', uri())),
 				m(Button, {
 					raised: true,
 					disabled: sending,
@@ -184,14 +189,72 @@ var OpenData = {
 						doRequest();
 					},
 				}, _('Send')),
-				m.trust('&nbsp;'),
-				_('Query: %{uri}', {uri: uri()}),
 			),
-            result && m('pre', jsyaml.dump(result)),
+			result && [
+				m('h2', 'Result'),
+				m(Select, {
+					id: 'view',
+					label: _('View Mode'),
+					help: _('How do you want to view it'),
+					required: true,
+					value: viewmode,
+					onchange: function(ev) {viewmode=ev.target.value;},
+					options: [{
+						text: _('Table'),
+						value: 'table',
+					},{
+						text: _('YAML (original)'),
+						value: 'yaml',
+					},{
+						text: _('JSON'),
+						value: 'json',
+					}],
+				}),
+				viewmode==='table' && resultTable(result),
+				viewmode==='yaml' && m('pre', jsyaml.dump(result)),
+				viewmode==='json' && m('pre', JSON.stringify(result,null,2)),
+			],
             apierror && m('pre.red', "Error: ", apierror.message),
         ]));
     },
 };
+
+var levels = ['countries', 'ccaas', 'states', 'cities']
+
+function resultTable(data) {
+	return m('table',
+		m('tr',
+			m('th', {colspan: levels.length+3}, _('Name')),
+			data.dates.map(function(date, i) {
+				return m('th', moment(date).format('YYYY-MM-DD'));
+
+			})
+		),
+		subresultTable(data,0)
+	);
+}
+
+
+function subresultTable(subresult, level) {
+	if (subresult===undefined) return [];
+	var children = levels[level] && subresult[levels[level]];
+	var name = subresult.name || _('Global');
+	var indent = levels.length - level + 1;
+	return [
+		m('tr', [
+			m('td', {colspan: level+1}),
+			m('td', children?m('span.fa.fa-icon.fa-plus'):''),
+			m('td', {colspan: indent}, name),
+			subresult.values.map(function(value, i) {
+				return m('td', {style: 'text-align: right'}, value);
+			}),
+		]),
+		children!==undefined &&
+		Object.keys(children).map(function(code, i) {
+			return subresultTable(children[code], level+1);
+		}),
+	];
+}
 
 
 window.onload = function() {
