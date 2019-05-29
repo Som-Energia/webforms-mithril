d37afce19e6b1383e6b97fa15966f9a2f625c0e8
David García Garzón <voki@canvoki.net>
2019-05-30 01:25:03 +0200

    gapminder: update data on deploy

--- deployopendata.sh
+++ deployopendata.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+wget https://opendata.somenergia.coop/v0.2/contracts/by/ccaa/monthly -O src/data/contracts_ccaa_monthly.yaml
+wget https://opendata.somenergia.coop/v0.2/members/by/ccaa/monthly -O src/data/members_ccaa_monthly.yaml
 npm run deploy-prod
-mv dist/index.html dist/gapminder.html
 cp dist/opendata.html dist/index.html
 scp dist/* somadmin@192.168.1.5:/var/www/opendata-ui/
 
