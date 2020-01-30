d6fc816f0e50e27eec737bc2273ea4e526627008
Mar Jene <mar.jene@somenergia.coop>
2020-01-30 14:15:25 +0100

    Jest snapshot examples

--- src/opendata.test.js
+++ src/opendata.test.js
@@ -1,41 +1,21 @@
 var m = require('mithril')
-var MyComponent = require("./my-component.js")
-var Button = require('./mdc/button.js');
-var OpenData = require('./opendata.js')
-describe("Positive control", function() {
-    test("My component", function() {
-        var c = m(MyComponent,{text: "Hello World!"})
-        expect(c.attrs['text']).toBe("Hello World!");
+var tidy = require('mithril-jest').tidy
+function MyComponent() {
+    return {
+        view: function (vnode) {
+            return m("div", vnode.attrs.text)
+        }
+    }
+}
+describe("Snapshot Example", function() {
+    test("Positive control", function() {
+        var c = m(MyComponent,{text: "Bye!"})
+        const html = tidy(c);
+        expect(html).toMatchSnapshot()
     })
-})
-describe("Button", function() {
-    test("btn", function() {
-        var rised =Button.view({attrs: {"raised":true}})
-        expect(rised.attrs.className).toBe("mdc-button mdc-button--raised");
-    })
-})
-describe("Opendata", function() {
-    var layout = OpenData.view()
-    test("layout", function(){
-        expect(layout.tag).toBe('div')
-        expect(layout.attrs.className).toBe('form mdc-typography')
-        expect(layout.children.length).toBe(1)   
-    })
-    var opendata = layout.children[0].children
-    test("First child: title", function() {
-        var title = opendata[0]
-        expect(title.tag).toBe('h1');
-        expect(title.text).toBe('OPENDATAUI_TITLE');
-    })
-    test("Second child: disclaimer", function() {
-        var disclaimer = opendata[1]
-        expect(disclaimer.tag).toBe('div');
-        expect(disclaimer.attrs.className).toBe('disclaimer')
-        expect(disclaimer.text).toBe('ALPHA_DISCLAIMER');
-    })
-    test("Child 3: Metric select", function() {
-        var metricSelect = opendata[2]
-        expect(metricSelect.attrs.id).toBe('metric')
-        expect(metricSelect.attrs.options).toStrictEqual([{text: 'Members',value:'members'},{text:'Contracts', value: 'contracts'} ]);
+    test("Negative control", function() {
+        var c = m(MyComponent,{text: "Hello!"})
+        const html = tidy(c);
+        expect(html).toMatchSnapshot()
     })
 })
