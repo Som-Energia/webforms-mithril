d2cebb081bad6cae0b5089476b7aa82c2e826601
Mar Jene <mar.jene@somenergia.coop>
2020-02-21 10:21:15 +0100

    updating data

--- deployopendata.sh
+++ deployopendata.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+deploy_user=$1
+deploy_server=$2
+deploy_path=$3
+
+function usage () {
+    echo "Usage: $0 user server path" 1>&2
+    exit 1;
+}
+
+if [ $# -ne 3 ];
+        then
+            usage;
+fi
 
 wget https://opendata.somenergia.coop/v0.2/contracts/by/ccaa/monthly -O src/data/contracts_ccaa_monthly.yaml
 wget https://opendata.somenergia.coop/v0.2/members/by/ccaa/monthly -O src/data/members_ccaa_monthly.yaml
 npm run deploy-prod
 cp dist/opendata.html dist/index.html
-scp dist/* somadmin@192.168.1.5:/var/www/opendata-ui/
+scp -P 2200 dist/* $deploy_user@$deploy_server:$deploy_path
 
 
