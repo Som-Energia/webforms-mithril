f104f84418eedd32680c504aa55a0d57c5682ab7
David García Garzón <david.garcia@somenergia.coop>
2021-02-10 18:32:25 +0100

    avoid override index.html in production (part2)

--- deployopendata.sh
+++ deployopendata.sh
@@ -16,7 +16,7 @@ fi
 wget https://opendata.somenergia.coop/v0.2/contracts/by/ccaa/monthly -O src/data/contracts_ccaa_monthly.yaml
 wget https://opendata.somenergia.coop/v0.2/members/by/ccaa/monthly -O src/data/members_ccaa_monthly.yaml
 npm run deploy-prod
-cp dist/opendata.html dist/index.html
+#cp dist/opendata.html dist/index.html # Not any more
 scp -P 2200 dist/* $deploy_user@$deploy_server:$deploy_path
 
 
