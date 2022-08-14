1212fc65a27540ba0a6424d71fc94993696e4b29
David García Garzón <david.garcia@somenergia.coop>
2022-08-14 08:38:38 +0200

    mdc-select: updated to mdc 12

--- src/mdc/select.js
+++ src/mdc/select.js
@@ -4,7 +4,11 @@
 var m = require('mithril');
 var _ = require('../translate');
 var MDCSelect = require('@material/select');
+require('@material/list/dist/mdc.list.css');
+require('@material/menu/dist/mdc.menu.css');
+require('@material/menu-surface/dist/mdc.menu-surface.css');
 require('@material/select/dist/mdc.select.css');
+require('./select.styl');
 
 /**
 Mithril component wrapping an MDC Select input field.
@@ -39,6 +43,12 @@ var Select = {
 		var mdcselect = vn.dom.querySelector('.mdc-select');
 		vn.state.native = mdcselect.querySelector('select');
 		vn.state.mdcinstance = new MDCSelect.MDCSelect(mdcselect);
+		vn.state.mdcinstance.listen('MDCSelect:change', (ev) => {
+			console.log(`Selected option at index ${vn.state.mdcinstance.selectedIndex} with value "${vn.state.mdcinstance.value}"`);
+			ev.target.value = vn.state.mdcinstance.value;
+			vn.attrs.onchange && vn.attrs.onchange(ev);
+			m.redraw()
+		});
 	},
 	view: function(vn) {
 		function floats() {
@@ -49,12 +59,16 @@ var Select = {
 			if (!vn.state.native.value) return true;
 			return false;
 		}
+		vn.state.mdcinstance && vn.state.mdcinstance.setValue(vn.attrs.value)
 		var attrs = Object.assign({}, vn.attrs);
 		function pop(o,k) { var r=o[k]; if (r!==undefined) { delete o[k];} return r; }
 		const options = vn.attrs.options || [];
 		const floating = floats()
 		const boxed = pop(attrs, 'boxed');
 		const outlined = pop(attrs, 'outlined');
+		const required = attrs.required;
+		const disabled = attrs.disabled;
+		const label = pop(attrs, 'label');
 		const help = pop(attrs, 'help');
 		const icon = pop(attrs, 'icon');
 		const faicon = pop(attrs, 'faicon');
@@ -63,112 +77,138 @@ var Select = {
 		const leadingfaicon = pop(attrs, 'leadingfaicon');
 		const leadingiconaction = pop(attrs, 'leadingiconaction');
 		const help_id = vn.attrs.id+'_help';
+		const label_id = vn.attrs.id+'_label';
+		const selectedValue = vn.attrs.value;
+		function option(v) {
+			if (v.group) {
+				return [
+					// TODO: Use v.text as separator
+					option({...v, disabled: true, group: false}),
+					v.group.map( (v,i)=> option(v)),
+				]
+			}
+			return m('li.mdc-list-item'
+				+(v.value==selectedValue?'.mdc-list-item--selected':'')
+				+(v.disabled?'.mdc-list-item--disabled':'')
+				, {
+					'aria-selected': v.value==selectedValue?"true":undefined,
+					'aria-disabled': v.disabled?"true":undefined,
+					'data-value': v.value,
+					role: "option",
+				},
+				m('span.mdc-list-item__ripple'),
+				(leadingicon || leadingfaicon) && m('span.mdc-list-item__graphic'),
+				v.text && m('span.mdc-list-item__text', v.text)
+			)
+		}
 		return m('', [
 			m('.mdc-select'+
 				(vn.attrs.disabled?'.mdc-select--disabled':'')+
 				(outlined?'.mdc-select--outlined':'')+
-				(boxed?'.mdc-select--box':'')+
+				(boxed?'.mdc-select--filled':'')+
 				(leadingicon?'.mdc-select--with-leading-icon':'')+
+				(leadingfaicon?'.mdc-select--with-leading-icon':'')+
 				(icon?'.mdc-select--with-trailing-icon':'')+
+				(required?'.mdc-select--required':'')+
+				(disabled?'.mdc-select--disabled':'')+
+				(label?'':'.mdc-select--no-label')+
 				'', {
-				style: {width: '100%'},
-				},[
-				leadingicon &&
-					m('i.mdc-select__icon.material-icons',
-						leadingiconaction && {
-							tabindex: 0,
-							role: 'button',
-							onclick: leadingiconaction,
-						},
-						leadingicon),
-				leadingfaicon &&
-					m('i.mdc-select__icon.fa.fa-'+leadingfaicon,
-						leadingiconaction && {
-							tabindex: 0,
-							role: 'button',
-							onclick: leadingiconaction,
-						}),
-				m('i.mdc-select__dropdown-icon'),
-				m('select'+
-				'.mdc-select__native-control'+
-				'', {
+					style: {width: '100%'},
+				}, [
+				m('input[type=hidden]', {
+					...vn.attrs,
 					id: vn.attrs.id,
 					required: vn.attrs.required, // TODO: current MDC version does not work yet
 					disabled: vn.attrs.disabled,
 					'aria-controls': help_id,
 					'aria-describedby': help_id,
 					value: vn.attrs.value,
-					onchange: function(ev) {
-						vn.attrs.value = ev.target.value;
-						vn.attrs.onchange && vn.attrs.onchange(ev);
-					},
 					oninvalid: function(ev) {
 						vn.attrs.oninvalid && vn.attrs.oninvalid(ev);
 					},
-				}, 
-					m('option', {
-						value:'', // label provides
-						disabled: vn.attrs.required,
-						selected: (vn.attrs.value === undefined) ? true : false 
-						}),
-					options.map(function (v,i) {
-						if (v.group) {
-							return m('optgroup', Object.assign({label: v.text}, v),
-								v.group.map(function(v,i) {
-									return m('option', Object.assign({},v), v.text);
-								}));
-						}
-						
-						return m('option', Object.assign({},v), v.text);
-					})
-				),
-				(vn.attrs.outlined?[]:m('label'
-					+'.mdc-floating-label'
-					+(floating?'.mdc-floating-label--float-above':''),
-					vn.attrs.label)),
-				vn.attrs.icon &&
-					m('i.mdc-select__icon.material-icons',
-						vn.attrs.iconaction && {
-							tabindex: 0,
-							role: 'button',
-							onclick: vn.attrs.iconaction,
-						},
-						vn.attrs.icon),
-				vn.attrs.faicon &&
-					m('i.mdc-select__icon.fa.fa-'+vn.attrs.faicon,
-						vn.attrs.iconaction && {
-							tabindex: 0,
-							role: 'button',
-							onclick: vn.attrs.iconaction,
-						}),
-				(vn.attrs.outlined? []: m('.mdc-line-ripple')),
-				(vn.attrs.outlined?
-					m('.mdc-notched-outline' +(floating?'.mdc-notched-outline--notched':''), [
-						m('.mdc-notched-outline__leading'),
-						m('.mdc-notched-outline__notch',
-							m('label.mdc-floating-label'
-								+(floating?'.mdc-floating-label--float-above':''),
-								vn.attrs.label)),
-						m('.mdc-notched-outline__trailing')
-					]):''),
-				/* Old version
-				vn.attrs.outlined && m('.mdc-notched-outline'
-					+(floating?'.mdc-notched-outline--notched':''),
-					m('svg', m('path.mdc-notched-outline__path'))),
-				vn.attrs.outlined && m('.mdc-notched-outline__idle'),
-				*/
+				}),
+				m('.mdc-select__anchor', {
+					role: "button",
+					'aria-haspopup': "listbox",
+					'aria-expanded': "false",
+					'aria-required': required?"true":undefined,
+					'aria-disabled': disabled?"true":undefined,
+					'aria-labelledby': label_id + " demo-selected-text",
+					'aria-controls': help_id,
+					'aria-describedby': help_id,
+					}, [
+						(boxed? [
+							m('span.mdc-select__ripple'),
+							label && m('span.mdc-floating-label'
+								+(vn.attrs.value?'.mdc-floating-label--float-above':'')
+								, {
+									id: label_id,
+								}, label),
+						]:''),
+						(outlined ? m('span.mdc-notched-outline', [
+							m('span.mdc-notched-outline__leading'),
+							label && m('span.mdc-notched-outline__notch',
+								m('span.mdc-floating-label'
+									+(vn.attrs.value?'.mdc-floating-label--float-above':'')
+									, label),
+							),
+							m('span.mdc-notched-outline__trailing'),
+						]):''),
+						leadingicon &&
+							m('i.mdc-select__icon.mdc-select__icon--leading.material-icons',
+								leadingiconaction && {
+									tabindex: 0,
+									role: 'button',
+									onclick: leadingiconaction,
+								},
+								leadingicon),
+						leadingfaicon &&
+							m('i.mdc-select__icon.mdc-select__icon--leading.fa.fa-'+leadingfaicon,
+								leadingiconaction && {
+									tabindex: 0,
+									role: 'button',
+									onclick: leadingiconaction,
+								}),
+						m('span.mdc-select__selected-text-container',
+							m('span.mdc-select__selected-text', {
+								id: 'demo-selected-text',
+							})
+						),
+						m('.mdc-select__dropdown-icon',
+							m('svg.mdc-select__dropdown-icon-graphic', {
+								viewBox: "7 10 10 5",
+								focusable: "false",
+							}, [
+								m('polygon.mdc-select__dropdown-icon-inactive', {
+									stroke: "none",
+									'fill-rule': "evenodd",
+									points: "7 10 12 15 17 10",
+								}),
+								m('polygon.mdc-select__dropdown-icon-active', {
+									stroke: "none",
+									'fill-rule': "evenodd",
+									points: "7 15 12 10 17 15",
+								}),
+							])
+						),
+						(boxed? m('.mdc-line-ripple'):[]),
+				]),
+				m('.mdc-select__menu.mdc-menu.mdc-menu-surface.mdc-menu-surface--fullwidth', [
+					m('ul.mdc-list[role=listbox]', [ // aria-label="Food picker listbox"
+						option({value: undefined, text: ''}, vn.state.value), // undefined value
+						options.map( (v,i)=> option(v, vn.state.value)),
+					]),
+				]),
 			]),
-			vn.attrs.nohelp === true ? []:
-			m('.mdc-text-field-helper-text'+
-				'.mdc-text-field-helper-text--persistent'+
-				'.mdc-text-field-helper-text--validation-msg'+
+			m('p.mdc-select-helper-text'+
+				//'.mdc-select-helper-text--validation-msg'+
 				'', {
 				'aria-hidden': true,
 				id: help_id,
 				},
-				vn.attrs.help
+				help
 			),
-		])
+		]);
 	},
 };
 
@@ -182,7 +222,6 @@ var Example = {
 		tastes: 'vegetables',
 	},
 	view: function(vn) {
-		var self = this;
 		const Layout = require('./layout');
 		const options = [
 			{
@@ -197,11 +236,17 @@ var Example = {
 				value: 'fruits',
 				text: _('Apple'),
 			},
+			{
+				value: 'canibalism',
+				text: _('Human Flesh'),
+				disabled: true,
+			},
 		];
 		return m(Layout, [
 			m(Layout.Row, m(Layout.Cell, {span:12},
 				m('h2', 'Selects'))),
 			m(Layout.Row,['boxed','outlined'].map(function(style) {
+			console.log(Example.Person)
 			return m(Layout.Cell,{span:4},
 				m(Select, {
 					boxed: style!='outlined',
@@ -211,24 +256,25 @@ var Example = {
 					help: _('Select what you like more'),
 					required: true,
 					//disabled: true,
-					value: self.Person.tastes,
+					value: Example.Person.tastes,
 					onchange: function(ev) {
-						vn.state.Person.tastes = ev.target.value;
+						Example.Person.tastes = ev.target.value;
 					},
 					options: options,
 				}));
 			}),
 			m(Layout.Cell,{span:4},
 				m(Select, {
-					//outlined: true,
+					boxed: true,
 					id: 'iconselect',
 					label: _('Tastes'),
 					help: _('Select what you like more'),
 					required: false,
 					leadingicon: 'restaurant',
-					value: self.Person.tastes,
+					value: Example.Person.tastes,
 					onchange: function(ev) {
-						vn.state.Person.tastes = ev.target.value;
+						console.log("Changed", ev.target.value)
+						Example.Person.tastes = ev.target.value;
 					},
 					options: options,
 				})),
@@ -243,9 +289,10 @@ var Example = {
 					leadingiconaction: function () {
 						console.log("Action");
 					},
-					value: self.Person.tastes,
+					value: Example.Person.tastes,
 					onchange: function(ev) {
-						vn.state.Person.tastes = ev.target.value;
+						console.log("Changed", ev.target.value)
+						Example.Person.tastes = ev.target.value;
 					},
 					options: options,
 				})),
@@ -260,15 +307,15 @@ var Example = {
 					iconaction: function () {
 						console.log("Action");
 					},
-					value: self.Person.tastes,
+					value: Example.Person.tastes,
 					onchange: function(ev) {
-						vn.state.Person.tastes = ev.target.value;
+						Example.Person.tastes = ev.target.value;
 					},
 					options: options,
 				}))
 			),
 			m(Layout.Row, m(Layout.Cell, {span:12},
-				_('You are fan of '), self.Person.tastes )),
+				_('You are fan of '), Example.Person.tastes )),
 		]);
 	},
 };
